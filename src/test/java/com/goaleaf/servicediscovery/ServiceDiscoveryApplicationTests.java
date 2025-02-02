836da9e0223f719ca741719d99f82b54ad10d0e9
pplociennik <pplociennik097@gmail.com>
2025-02-02 17:43:18 +0100

    [PPGLF-8, PPGLF-9, PPGLF-10, PPGLF-11] Log Aggregation, Micrometer support, Prometheus support

---
+++ src/test/java/com/goaleaf/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goaleaf.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
